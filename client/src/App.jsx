860f17d488e4265520cc45454627fc0f36c4876f
GitHubNexon <johnmarkpulmano052802@gmail.com>
2025-05-08 15:44:22 +0800

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,11 @@ import { useState } from "react";
 import { FaReact } from "react-icons/fa";
 import { RiTailwindCssFill } from "react-icons/ri";
 import Home from "./pages/Home";
-import Test from "./pages/Test";
 
 function App() {
   return (
     <>
-      {/* <Home /> */}
-      <Test />
+      <Home />
     </>
   );
 }
